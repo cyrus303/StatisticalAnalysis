752018757ef039044a6e0e02ec76b95e92b81bd2
cyrus303 <cyrus303@users.noreply.github.com>
2019-12-22 22:45:10 +0000

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -140,4 +140,7 @@ def channel_results(reduced_data, outliers, pca_samples):
 	channel = channel.drop(channel.index[outliers]).reset_index(drop = True)
 	labeled = pd.concat([reduced_data, channel], axis = 1)
 	
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
 	
